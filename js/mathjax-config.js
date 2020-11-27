9260b54221aec8d9e219f35a9744438a500e8b1d
liuyang12 <yliu@csail.mit.edu>
2020-11-27 04:01:50 -0500

    updated intro, paper list and cv, fall 2020

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -1,16 +1,10 @@
-// MathJax Configuration
-//
-// v2 to v3 upgrade notes:
-// - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
-// - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
 window.MathJax = {
-  tex: {
+  CommonHTML: {linebreaks: {automatic: true}},
+  tex2jax: {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     displayMath: [['$$', '$$'], ['\\[', '\\]']],
-    processEscapes: false,
-    packages: {'[+]': ['noerrors']}
+    processEscapes: false
   },
-  loader: {
-    load: ['[tex]/noerrors']
-  }
+  TeX: {noUndefined: {attributes: {mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%'}}},
+  messageStyle: 'none'
 };
